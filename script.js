4426714dccea6f0fab4a07fff8b2e64de0107c29
gracekemper <125609501+gracekemper@users.noreply.github.com>
2023-04-11 17:20:20 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,51 @@
-const quizForm = document.getElementById('quiz-form');
+// Define the quiz questions and their answers
+const quizQuestions = [
+	{
+		question: "How often are you hungry?",
+		answers: ["Always", "Often", "Sometimes", "Never"]
+	},
+	{
+		question: "What's your favorite pet?",
+		answers: ["Dog", "Cat", "Hamster", "Snake", "Bird", "Chicken"]
+	},
+	{
+		question: "What's your favorite weather?",
+		answers: ["Cloudy", "Rainy", "Sunny", "Foggy"]
+	},
+	{
+		question: "Would you consider yourself...",
+		answers: ["Extrovert", "Introvert", "Ambivert"]
+	},
+	{
+		question: "What's your zodiac?",
+		answers: ["Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Saggitarius", "Capricorn", "Aquarius", "Pisces"]
+	}
+];
 
-quizForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const resultsContainer = document.createElement('div');
-  resultsContainer.classList.add('results-container');
-  const resultsText = document.createElement('p');
-  resultsText.classList.add('results-text');
-  resultsText.textContent = 'Your result: Blank Result';
-  resultsContainer.appendChild(resultsText);
-  quizForm.appendChild(resultsContainer);
-  window.scrollTo(0, document.body.scrollHeight);
+// Define the result messages
+const resultTitle = "You are blank";
+const resultDescription = "Everyone is blank.";
+
+// Get the quiz form and result div
+const quizForm = document.getElementById("quiz-form");
+const resultDiv = document.getElementById("result");
+
+// Add event listener to the form submission
+quizForm.addEventListener("submit", function(event) {
+	event.preventDefault();
+	
+	// Get the user's answers from the form
+	const userAnswers = [];
+	for (let i = 0; i < quizQuestions.length; i++) {
+		const selectedAnswer = document.querySelector(`input[name="question${i}"]:checked`);
+		if (!selectedAnswer) {
+			alert("Please answer all questions before submitting.");
+			return;
+		}
+		userAnswers.push(selectedAnswer.value);
+	}
+	
+	// Display the result on a new page
+	const resultUrl = `result.html?resultTitle=${resultTitle}&resultDescription=${resultDescription}`;
+	window.location.href = resultUrl;
 });
